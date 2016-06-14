1579aa831e77f64d2134acba20a2c9ecc24e372a
nik <nik_rb@yahoo.com>
2016-06-14 03:17:28 +0000

    add api request

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import style from '../scss/app.scss';
+import http from 'http';
 
 // require('../../node_modules/bootstrap/dist/css/bootstrap.css');
 
 // es5
 var Main = React.createClass({
+  getInitialState: function(){
+    console.log( "@getInitialState");
+    return { data: []};
+  },
+  loadData: function(){
+    console.log( "@loadData");
+    http.get( '/api/comments' , function( res){
+      console.log( "got response:", res);
+      this.setState( { data: res});
+    });
+  },
+  componentDidMount(){
+    console.log( "@componentDidMount");
+    this.loadData();
+  },
   render: function(){
     return (
       <div className="example container">
-        <h1>This is one cool app!</h1>
+        <h1>This is two cool app!</h1>
+        <p>{this.state.data}</p>
       </div>
     );
   }
